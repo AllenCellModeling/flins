39d9e54511692329b81d329099c4c0df109bef61
Dave Williams <cdave@uw.edu>
2019-03-08 16:33:02 -0800

    Black formatting

--- stress_fiber/_units.py
+++ stress_fiber/_units.py
@@ -12,8 +12,11 @@ def poise(input_in_poise):
 
 def joules(input_in_joules):
     """Convert energy, in joules, to pN*nm"""
-    return input_in_joules * 10 ** 21
+    return input_in_joules * 10**21
 
+def kcal(input_in_kcal):
+    """Convert energy, in kcal, to pN*nm"""
+    return input_in_kcal * 4.184 * 10**24
 
 def milliseconds(input_in_ms):
     """Convert time, in ms, to seconds"""
@@ -24,6 +27,6 @@ class constants:
     temperature = 288  # Standard temperature, K
     eta = poise(0.0114)  # Viscosity of water at 288K
     timestep = milliseconds(1)  # 1ms timestep
-    boltzmann = joules(1.38 * 10 ** -23)  # From J/K to pN*nm/K
+    boltzmann = joules(1.38 * 10**-23)  # From J/K to pN*nm/K
     kT = boltzmann * temperature
     avogadro = 6.022 * 10**23
