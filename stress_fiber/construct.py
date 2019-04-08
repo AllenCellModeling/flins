7dd7c9bcb0eff765e0f523bf8531fbcceeb84d59
Dave Williams <cdave@uw.edu>
2019-04-08 11:12:14 -0700

    Add anchors in test world
    To actins in first and last tenths of space.

--- stress_fiber/construct.py
+++ stress_fiber/construct.py
@@ -9,6 +9,7 @@ import numpy as np
 
 from . import space
 from .actin import Actin
+from .anchor import Anchor
 from .alpha_actinin import AlphaActinin
 
 np.random.seed()  # Ensure proper seeding
@@ -19,15 +20,23 @@ def create_test_world(radius, span, n_actin, n_actinin):
     tractspace = space.TractSpace(radius)
     for tract in tractspace.all_tracts:
         actins = []
+        anchors = []
         for _ in range(n_actin):
             rise = 2.77
             pairs = np.random.randint(4, span // rise - 4)
             x = np.random.rand() * (span - pairs * rise)
             actins.append(Actin(x, pairs, tract))
+            # Anchor first and last tenth
+            if x < span*0.1:
+                anchors.append(Anchor(x, actins[-1].pairs[0]))
+            x_end = actins[-1].pairs_x[-1]
+            if x_end > span*0.9:
+                anchors.append(Anchor(x_end, actins[-1].pairs[-1]))
         actinins = []
         for _ in range(n_actinin):
             actinins.append(AlphaActinin(np.random.rand() * (span - 10), tract))
         tract.mols["actin"] = actins
+        tract.mols["anchor"] = anchors
         tract.mols["actinin"] = actinins
     world = World(tractspace)
     return world
