aff8e76a4b3ee812b514ccb3264f03064b5e3907
Dave Williams <cdave@uw.edu>
2019-04-29 11:11:47 -0700

    Add scipy to requirements b/c we use scipy.optimize

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'matplotlib', 'svgwrite']
+requirements = ['numpy', 'scipy', 'matplotlib', 'svgwrite']
 
 setup_requirements = ['pytest-runner', ]
 
