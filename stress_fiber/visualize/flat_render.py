de0e48248a233159089a66b2a7c4cefdb18ae991
Dave Williams <cdave@uw.edu>
2019-06-12 11:10:12 -0700

    Improve SVG rendering
    When binding happens within a tract, directly link the sites. When binding happens on only one side of an actinin, attach to the filament but don't have a linking line.

--- stress_fiber/visualize/flat_render.py
+++ stress_fiber/visualize/flat_render.py
@@ -11,64 +11,88 @@ import numpy as np
 import svgwrite
 
 
-# Can globally set the style of the render via css
+# Can globally set the style of the render via CSS
 CSS_STYLES = """
     .background { background-color: oldlace; }
     .actin { stroke: cornflowerblue; stroke-width: 3px; }
     .anchor { stroke: tan; fill: tan;}
     .actinin { stroke: limegreen; fill: limegreen; stroke-width: 2px; }
     .tract { stroke: darkslategray; fill: whitesmoke; stroke-width: 3px; }
-    .fade {opacity: 0.3;}
+    .clear { fill-opacity: 0.0; }
+    .fade { opacity: 0.3; }
 """
 
 
-def _plot_anchor(anchor, group, y, params):
+def _plot_anchor(anchor, params):
     """Plot anc as part of group at y"""
     x = anchor.x * params["xm"]
+    y = anchor.bs.linked.filament.__y * params["ym"]
+    group = anchor.bs.linked.filament.tract.__groups['anchor']
     group.add(svgwrite.shapes.Circle((x, y), 4, class_="anchor"))
     return
 
 
-def _plot_actinin_head(actinin_head, group, y, params):
-    """Plot actinin as part of group from y[0] to y[1]"""
-    x_i = actinin_head.x * params["xm"]
-    if actinin_head.other_head.bs.bound:
-        x_f = actinin_head.other_head.x * params["xm"]
-    else:
-        x_f = x_i
-    group.add(svgwrite.shapes.Line((x_i, y[0]), (x_f, y[1]), class_="actinin"))
-    group.add(svgwrite.shapes.Circle((x_i, y[0]), 2, class_="actinin"))
+def _plot_actinin(actinin, y, params):
+    """Plot an actinin as part of group within y_lim"""
+    if not actinin.bound:  # none bound
+        y *= params["ym"]
+        x = actinin.x * params["xm"]
+        group = actinin.tract.__groups["unbound_actinin"]
+        group.add(svgwrite.shapes.Ellipse((x, y), (3, 1)))
+        return
+    if sum([h.bs.bound for h in actinin.heads]) == 2:  # both bound
+        _plot_actinin_body(actinin, params)
+    for head in actinin.heads:
+        _plot_actinin_head(head, params)
     return
 
 
-def _plot_actinin(actinin, group, y, params):
-    """Plot an actinin as part of group within y_lim"""
-    y *= params["ym"]
-    x = actinin.x * params["xm"]
-    group.add(svgwrite.shapes.Ellipse((x, y), (3, 1)))
+def _plot_actinin_head(head, params):
+    """Plot actinin head on actin"""
+    if not head.bs.bound:
+        return
+    x = head.x * params["xm"]
+    y = head.bs.linked.filament.__y * params["ym"]
+    group = head.bs.linked.filament.tract.__groups["bound_actinin"]
+    group.add(svgwrite.shapes.Circle((x, y), 2, class_="actinin"))
+    return
 
 
-def _plot_actin(actin, group, y, params):
+def _plot_actinin_body(actinin, params):
+    """Only plot body in case where both heads are bound"""
+    tracts = [head.bs.linked.filament.tract for head in actinin.heads]
+    if tracts[0] == tracts[1]:  # same tract
+        x = [head.x * params["xm"] for head in actinin.heads]
+        y = [h.bs.linked.filament.__y * params["ym"] for h in actinin.heads]
+        group = tracts[0].__groups["bound_actinin"]
+        group.add(svgwrite.shapes.Line((x[0], y[0]), (x[1], y[1]), class_="actinin"))
+    else:  # different tracts
+        for head, tract in zip(actinin.heads, tracts):
+            x_i = head.x * params["xm"]
+            x_f = head.other_head.x * params["xm"]
+            ym, y_span = params["ym"], params["y_span"]
+            y = head.bs.linked.filament.__y 
+            y_i = y * ym
+            y_f = np.round(y / y_span) * y_span * ym  # end links at top or bottom
+            group = tract.__groups["bound_actinin"]
+            group.add(svgwrite.shapes.Line((x_i, y_i), (x_f, y_f), class_="actinin"))
+        return 
+
+
+def _plot_actin(actin, params):
     """Plot actin and affiliated proteins as part of group within y_lim"""
     # Locally load params
     xm = params["xm"]
     ym = params["ym"]
     y_span = params["y_span"]
+    y = actin.__y
+    group = actin.tract.__groups["actin"]
     # Find limits and convert to user units
     y_edge = np.round(y / y_span) * y_span * ym  # end links at top or bottom
     x = np.multiply(xm, actin.boundaries)
     y *= ym
     # Plot actin
     group.add(svgwrite.shapes.Line((x[0], y), (x[1], y)))
-    # Plot associated mols
-    for pair in actin.pairs:
-        if not pair.bs.bound:
-            continue
-        mol = pair.bs.linked
-        if type(mol) is sf.proteins.alpha_actinin.ActininHead:
-            _plot_actinin_head(mol, group, (y, y_edge), params)
-        elif type(mol) is sf.proteins.anchor.Anchor:
-            _plot_anchor(mol, group, y, params)
     return
 
 
@@ -86,12 +110,23 @@ def _plot_tract(dwg, tract, tract_i, params):
         svgwrite.shapes.Rect(size=(dwg.attribs["width"], ym * y_span), class_="tract")
     )
     group.translate(0, tract_y_offset)
+    tract.__groups = {
+        "self": group,
+        "actin": group.add(svgwrite.container.Group(class_="actin")),
+        "anchor": group.add(svgwrite.container.Group(class_="anchor")),
+        "bound_actinin": group.add(svgwrite.container.Group(class_="actinin")),
+        "unbound_actinin": group.add(svgwrite.container.Group(class_="actinin fade")),
+    }
+    group.add(
+        svgwrite.shapes.Rect(size=(dwg.attribs["width"], ym * y_span),
+                             class_="clear tract")
+    )
     return group
 
 
 def plot_world(world, params={}):
     """Plot a world as a flat, unrolled, svg
-    Paramters
+    Parameters
     ---------
     world: stress_fiber.construct.World
         World to render as svg
@@ -120,19 +155,26 @@ def plot_world(world, params={}):
     dwg = svgwrite.Drawing(size=(xm * x_span, ym * y_tot), class_="background")
     dwg.defs.add(dwg.style(CSS_STYLES))  # lets us use class defs from CSS_STYLES
     # Create each tract and plot contents
+    # Tracts
     for i, tract in enumerate(world.tractspace.all_tracts):
-        tract_group = _plot_tract(dwg, tract, i, params)
-        # Plot unbound actinin
-        n_actinin = len(tract.mols["actinin"])
-        actinin_group = tract_group.add(svgwrite.container.Group(class_="actinin fade"))
-        for j, actinin in enumerate(tract.mols["actinin"]):
+        _plot_tract(dwg, tract, i, params)
+    # Actins
+    for tract in world.tractspace.all_tracts:
+        actins = tract.mols["actin"]
+        n_actin = len(actins)
+        for k, actin in enumerate(actins):
+            actin.__y = y_span * (k + 1) / (n_actin + 1)
+            _plot_actin(actin, params)
+    # Alpha-actinins
+    for tract in world.tractspace.all_tracts:
+        actinins = tract.mols["actinin"]
+        n_actinin = len(actinins)
+        for j, actinin in enumerate(actinins):
             y_actinin = y_span * (j + 1) / (n_actinin + 1)
-            if not actinin.bound:
-                _plot_actinin(actinin, actinin_group, y_actinin, params)
-        # Plot Actin and associated
-        n_actin = len(tract.mols["actin"])
-        actin_group = tract_group.add(svgwrite.container.Group(class_="actin"))
-        for k, act in enumerate(tract.mols["actin"]):
-            y_act = y_span * (k + 1) / (n_actin + 1)
-            _plot_actin(act, actin_group, y_act, params)
+            _plot_actinin(actinin, y_actinin, params)
+    # Anchors
+    for tract in world.tractspace.all_tracts:
+        if "anchor" in tract.mols:
+            for anchor in tract.mols["anchor"]:
+                _plot_anchor(anchor, params)
     return dwg
