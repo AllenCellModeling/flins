6034cc2f93d3a45207f36f9b400a9b4acd07982c
Dave Williams <cdave@uw.edu>
2019-06-25 13:38:30 -0700

    Black formatting of docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 import sphinx_rtd_theme
 import stress_fiber
@@ -47,22 +48,22 @@ napoleon_use_ivar = True
 napoleon_use_param = False
 
 # Control autodoc
-autoclass_content = 'both'  # include init doc with class
+autoclass_content = "both"  # include init doc with class
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'stress_fiber'
+project = u"stress_fiber"
 copyright = u"2019, Dave Williams"
 author = u"Dave Williams"
 
@@ -85,10 +86,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -110,13 +111,13 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'stress_fiberdoc'
+htmlhelp_basename = "stress_fiberdoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -125,15 +126,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -143,9 +141,13 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'stress_fiber.tex',
-     u'stress_fiber Documentation',
-     u'Dave Williams', 'manual'),
+    (
+        master_doc,
+        "stress_fiber.tex",
+        u"stress_fiber Documentation",
+        u"Dave Williams",
+        "manual",
+    )
 ]
 
 
@@ -153,11 +155,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'stress_fiber',
-     u'stress_fiber Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "stress_fiber", u"stress_fiber Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -166,13 +164,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'stress_fiber',
-     u'stress_fiber Documentation',
-     author,
-     'stress_fiber',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "stress_fiber",
+        u"stress_fiber Documentation",
+        author,
+        "stress_fiber",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
-
-
-
