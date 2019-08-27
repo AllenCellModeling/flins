8e9360ec187dc7009f45f12ce93bbb7aee63417e
Dave Williams <cdave@uw.edu>
2019-08-27 13:18:27 -0700

    Swap actinin and actinin-head locations

--- stress_fiber/proteins/alpha_actinin.py
+++ stress_fiber/proteins/alpha_actinin.py
@@ -18,6 +18,133 @@ from ..support import kinetics
 from ..support import binding_site
 
 
+class AlphaActinin(Protein):
+    """A 1D α-actinin molecule with heads on either end
+
+    As this head is considered as a semi-physical object, we care about its
+    dimensions and material properties. The dimensions of α-actinin are readily
+    available from crystallization, and more recently cryo-EM, studies.
+
+    =========  =======  ======  ==========
+    Property   Value    Units   Source
+    =========  =======  ======  ==========
+    Length     24-36    nm      [1]_, [2]_
+    Thickness  0.5-6.5  nm      [1]_, [2]_
+    =========  =======  ======  ==========
+
+    Stiffness values are far harder to find. The flexural stiffness and
+    persistence length of α-actinin have been measured or calculated via
+    simulation multiple times, but I am unable to locate an extensional
+    stiffness. So, let's calculate the Young's modulus from persistence length
+    measurements made on generic α-helices and translate that to the specific
+    dimensions of α-actinin. We take the persistence length of an α-helix to be
+    80nm [3]_, and therefore for our tetrameric spectrin repeat backbone to be
+    240nm. This assumes linear scaling, but is supported in [3]_ for scaling
+    from single helices to coiled-coils. More structural detail can be found,
+    amongst other places, in Autore_2013_.
+
+    Persistence length, :math:`L_p`, is related to the bending stiffness,
+    :math:`B_s`, and therefore to Young's modulus, :math:`E`, via
+    :math:`L_p=\\frac{B_s}{k_B T}=\\frac{E I}{k_B T}` where :math:`I` is the
+    second moment of area. For a rod :math:`I=\\frac{\pi r^4}{4}`.  So our
+    :math:`E` becomes :math:`E=\\frac{4 L_p k_B T}{\pi r^4}`. What we really
+    want is the spring constant, :math:`k` which we get from :math:`E` by
+    :math:`k=\\frac{E \pi r^2}{L_0}` and thus
+
+    .. math:: k = \\frac{4 L_p k_B T}{r^2 L_0}
+
+    Where with :math:`L_p=240nm`, :math:`k_B=0.0138pN*nm/K`, :math:`T=288K`,
+    :math:`r=1.5nm`, and :math:`L_0=36nm` to give :math:`k=3.75pN/nm`
+
+    While we'll use this extensional stiffness below, it is worth noting that
+    bending may be the primary mechanism by which α-actinin deforms (Grum_1999_).
+
+    =========  =======  ======  ==========
+    Property   Value    Units   Source
+    =========  =======  ======  ==========
+    Stiffness  3.75     pN/nm   [3]_
+    =========  =======  ======  ==========
+
+    .. [1] https://dx.doi.org/10.1007/s00018-008-8080-8
+    .. [2] https://dx.doi.org/10.1016%2Fj.cell.2014.10.056
+    .. [3] https://dx.doi.org/10.1103/PhysRevLett.97.248101
+    .. _Autore_2013: https://dx.doi.org/10.1371/journal.pone.0063633
+    .. _Grum_1999: https://doi.org/10.1016/S0092-8674(00)81980-7
+    """
+
+    def __init__(self, x, tract=None):
+        """An α-actinin at location x in a tract"""
+        # Store tract if given, else create placeholders
+        super().__init__("actinin", tract)
+        # Create the spring that is our actinin and remember passed values
+        self.spring = spring.Spring(3.75, 36)  # See class doc for sources
+        self.x = x
+        # Create the heads at either end of the actinin
+        self.heads = [ActininHead(self, 0), ActininHead(self, 1)]
+
+    def __str__(self):
+        """String representation of α-actinin"""
+        tract_str = "NA" if self.tract is None else str(self.tract.loc)
+        x_str = "%.1f" % self.x
+        bound_str = "%i bound heads" % sum([h.bs.bound for h in self.heads])
+        return "α-act with %s at x=%s on tract %s" % (bound_str, x_str, tract_str)
+
+    @property
+    def bound(self):
+        """Are you bound?"""
+        return self.heads[0].bs.bound or self.heads[1].bs.bound
+
+    @property
+    def fully_bound(self):
+        """Are both sides bound?"""
+        return self.heads[0].bs.bound and self.heads[1].bs.bound
+
+    @property
+    def energy(self):
+        """Current energy born by the stretched (or not) α-actinin"""
+        if not (self.heads[0].bs.bound and self.heads[1].bs.bound):
+            return 0
+        dist = np.abs(self.heads[0].x - self.heads[1].x)
+        return self.spring.energy(dist)
+
+    def step(self):
+        """Take a timestep"""
+        if not self.bound:
+            self.freely_diffuse()
+        else:
+            if self.heads[0].bs.bound:
+                self.x = self.heads[0].bs.linked.x
+            else:
+                self.x = self.heads[1].bs.linked.x - self.spring.rest
+        [head.step() for head in self.heads]
+        return
+
+    def freely_diffuse(self):
+        """ Diffuse to a new location.
+        We know the approximate dimensions of the α-actinin backbone are 24-36
+        nm by 0.5-6.5 nm as specified in Sjöblom_2008_ and Ribeiro_2014_. We
+        treat this as an ellipsoid of radii 18 by 3 nm to account for the heads
+        at either end. 
+
+        An alternate treatment would be to use the Stoke's radius for actinin
+        described in BNID_104395_.
+
+        .. _Sjöblom_2008: https://dx.doi.org/10.1007/s00018-008-8080-8
+        .. _Ribeiro_2014: https://dx.doi.org/10.1016%2Fj.cell.2014.10.056
+        .. _BNID_104395: https://bionumbers.hms.harvard.edu/bionumber.aspx?id=104395
+        """
+        # NB This is checked well at this time, CDW20190221
+        b, a = 18, 3
+        f_drag = diffuse.Drag.Ellipsoid.long_axis_translation(b, a)
+        d_x = diffuse.Dx(f_drag)
+        self.x += d_x
+        if self.tract is not None:  # then derive diffusion limits from tract
+            start, end = self.x, self.x + self.spring.rest
+            space_limits = (0, self.tract.space.span)
+            self.x, _ = diffuse.coerce_to_bounds(start, end, space_limits)
+        return d_x
+
+
 class ActininHead:
     """One of the two heads of an α-actinin"""
 
@@ -185,130 +312,3 @@ class ActininHead:
         """
         energy_fn = self.actinin.spring.energy
         return self._spring_property(energy_fn, x)
-
-
-class AlphaActinin(Protein):
-    """A 1D α-actinin molecule with heads on either end
-
-    As this head is considered as a semi-physical object, we care about its
-    dimensions and material properties. The dimensions of α-actinin are readily
-    available from crystallization, and more recently cryo-EM, studies.
-
-    =========  =======  ======  ==========
-    Property   Value    Units   Source
-    =========  =======  ======  ==========
-    Length     24-36    nm      [1]_, [2]_
-    Thickness  0.5-6.5  nm      [1]_, [2]_
-    =========  =======  ======  ==========
-
-    Stiffness values are far harder to find. The flexural stiffness and
-    persistence length of α-actinin have been measured or calculated via
-    simulation multiple times, but I am unable to locate an extensional
-    stiffness. So, let's calculate the Young's modulus from persistence length
-    measurements made on generic α-helices and translate that to the specific
-    dimensions of α-actinin. We take the persistence length of an α-helix to be
-    80nm [3]_, and therefore for our tetrameric spectrin repeat backbone to be
-    240nm. This assumes linear scaling, but is supported in [3]_ for scaling
-    from single helices to coiled-coils. More structural detail can be found,
-    amongst other places, in Autore_2013_.
-
-    Persistence length, :math:`L_p`, is related to the bending stiffness,
-    :math:`B_s`, and therefore to Young's modulus, :math:`E`, via
-    :math:`L_p=\\frac{B_s}{k_B T}=\\frac{E I}{k_B T}` where :math:`I` is the
-    second moment of area. For a rod :math:`I=\\frac{\pi r^4}{4}`.  So our
-    :math:`E` becomes :math:`E=\\frac{4 L_p k_B T}{\pi r^4}`. What we really
-    want is the spring constant, :math:`k` which we get from :math:`E` by
-    :math:`k=\\frac{E \pi r^2}{L_0}` and thus
-
-    .. math:: k = \\frac{4 L_p k_B T}{r^2 L_0}
-
-    Where with :math:`L_p=240nm`, :math:`k_B=0.0138pN*nm/K`, :math:`T=288K`,
-    :math:`r=1.5nm`, and :math:`L_0=36nm` to give :math:`k=3.75pN/nm`
-
-    While we'll use this extensional stiffness below, it is worth noting that
-    bending may be the primary mechanism by which α-actinin deforms (Grum_1999_).
-
-    =========  =======  ======  ==========
-    Property   Value    Units   Source
-    =========  =======  ======  ==========
-    Stiffness  3.75     pN/nm   [3]_
-    =========  =======  ======  ==========
-
-    .. [1] https://dx.doi.org/10.1007/s00018-008-8080-8
-    .. [2] https://dx.doi.org/10.1016%2Fj.cell.2014.10.056
-    .. [3] https://dx.doi.org/10.1103/PhysRevLett.97.248101
-    .. _Autore_2013: https://dx.doi.org/10.1371/journal.pone.0063633
-    .. _Grum_1999: https://doi.org/10.1016/S0092-8674(00)81980-7
-    """
-
-    def __init__(self, x, tract=None):
-        """An α-actinin at location x in a tract"""
-        # Store tract if given, else create placeholders
-        super().__init__("actinin", tract)
-        # Create the spring that is our actinin and remember passed values
-        self.spring = spring.Spring(3.75, 36)  # See class doc for sources
-        self.x = x
-        # Create the heads at either end of the actinin
-        self.heads = [ActininHead(self, 0), ActininHead(self, 1)]
-
-    def __str__(self):
-        """String representation of α-actinin"""
-        tract_str = "NA" if self.tract is None else str(self.tract.loc)
-        x_str = "%.1f" % self.x
-        bound_str = "%i bound heads" % sum([h.bs.bound for h in self.heads])
-        return "α-act with %s at x=%s on tract %s" % (bound_str, x_str, tract_str)
-
-    @property
-    def bound(self):
-        """Are you bound?"""
-        return self.heads[0].bs.bound or self.heads[1].bs.bound
-
-    @property
-    def fully_bound(self):
-        """Are both sides bound?"""
-        return self.heads[0].bs.bound and self.heads[1].bs.bound
-
-    @property
-    def energy(self):
-        """Current energy born by the stretched (or not) α-actinin"""
-        if not (self.heads[0].bs.bound and self.heads[1].bs.bound):
-            return 0
-        dist = np.abs(self.heads[0].x - self.heads[1].x)
-        return self.spring.energy(dist)
-
-    def step(self):
-        """Take a timestep"""
-        if not self.bound:
-            self.freely_diffuse()
-        else:
-            if self.heads[0].bs.bound:
-                self.x = self.heads[0].bs.linked.x
-            else:
-                self.x = self.heads[1].bs.linked.x - self.spring.rest
-        [head.step() for head in self.heads]
-        return
-
-    def freely_diffuse(self):
-        """ Diffuse to a new location.
-        We know the approximate dimensions of the α-actinin backbone are 24-36
-        nm by 0.5-6.5 nm as specified in Sjöblom_2008_ and Ribeiro_2014_. We
-        treat this as an ellipsoid of radii 18 by 3 nm to account for the heads
-        at either end. 
-
-        An alternate treatment would be to use the Stoke's radius for actinin
-        described in BNID_104395_.
-
-        .. _Sjöblom_2008: https://dx.doi.org/10.1007/s00018-008-8080-8
-        .. _Ribeiro_2014: https://dx.doi.org/10.1016%2Fj.cell.2014.10.056
-        .. _BNID_104395: https://bionumbers.hms.harvard.edu/bionumber.aspx?id=104395
-        """
-        # NB This is checked well at this time, CDW20190221
-        b, a = 18, 3
-        f_drag = diffuse.Drag.Ellipsoid.long_axis_translation(b, a)
-        d_x = diffuse.Dx(f_drag)
-        self.x += d_x
-        if self.tract is not None:  # then derive diffusion limits from tract
-            start, end = self.x, self.x + self.spring.rest
-            space_limits = (0, self.tract.space.span)
-            self.x, _ = diffuse.coerce_to_bounds(start, end, space_limits)
-        return d_x
